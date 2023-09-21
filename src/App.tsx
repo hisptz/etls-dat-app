19dc657be7016bff8b7b0a9a1f2bf1b7ea7d6b3d
Gift Nnko <nnkogift@hotmail.com>
2023-09-21 07:06:47 +0300

    chore(*): Project setup
    - Initialized project with d2 script
    - Setup typescript
    - Setup project running config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './App.module.css'
+
+const MyApp = () => (
+		<div className={classes.container}>
+				<h1>DAT Custom app</h1>
+		</div>
+)
+
+export default MyApp
