c8d3bd1bdb2fc0b1178fdc3876930bcdda59a624
Davis799 <davistibz@gmail.com>
2024-01-11 17:19:54 +0300

    feat(alarms): feat
    - Improved the alarms ui as per the new requirements

--- src/modules/shared/components/ProfileArea/AddAlarm.tsx
+++ src/modules/shared/components/ProfileArea/AddAlarm.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	Button,
 	Modal,
 	ModalTitle,
 	ModalContent,
 	ModalActions,
+	Switch,
 	ButtonStrip,
+	Checkbox,
 } from "@dhis2/ui";
 import i18n from "@dhis2/d2-i18n";
+import styles from "./ProfileArea.module.css";
 import { FilterField } from "../../../Configuration/components/ProgramMapping/components/FilterField";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -27,14 +30,14 @@ interface addAlarmProps {
 	device: string;
 }
 
-export const daysInWeek: Option[] = [
-	{ name: "Sunday", code: "1" },
-	{ name: "Monday", code: "2" },
-	{ name: "Tuesday", code: "3" },
-	{ name: "Wednesday", code: "4" },
-	{ name: "Thursday", code: "5" },
-	{ name: "Friday", code: "6" },
-	{ name: "Saturday", code: "7" },
+export const daysInWeek: any = [
+	{ name: "Sun", code: "1" },
+	{ name: "Mon", code: "2" },
+	{ name: "Tue", code: "3" },
+	{ name: "Wed", code: "4" },
+	{ name: "Thur", code: "5" },
+	{ name: "Fri", code: "6" },
+	{ name: "Sat", code: "7" },
 ];
 
 function EditAlarm({
@@ -49,24 +52,27 @@ function EditAlarm({
 	device,
 }: addAlarmProps) {
 	const { setAlarm } = useSetAlarm();
+	const [doseReminder, setDoseReminder] = useState<boolean>(false);
+	const [appointmentReminder, setAppointmentReminder] =
+		useState<boolean>(false);
 
 	const schema = z.object({
 		dayInWeek:
 			frequency == "Weekly"
 				? z
-					.string({
-						required_error: "Day of the Dose is required",
-					})
-					.nonempty("Day of the Dose is required")
+						.string({
+							required_error: "Day of the Dose is required",
+						})
+						.nonempty("Day of the Dose is required")
 				: z.string(),
 		nextDoseTime:
 			frequency == "Monthly"
 				? z.string()
 				: z
-					.string({
-						required_error: "Next Dose Time is required",
-					})
-					.nonempty("Next Dose Time is required"),
+						.string({
+							required_error: "Next Dose Time is required",
+						})
+						.nonempty("Next Dose Time is required"),
 		nextRefillDate: z
 			.string({ required_error: "Next Refill Date is required" })
 			.nonempty("Next Refill Date is required"),
@@ -108,8 +114,8 @@ function EditAlarm({
 				frequency == "Daily"
 					? "1111111"
 					: frequency == "Weekly"
-						? generateDays(data.dayInWeek)
-						: null,
+					? generateDays(data.dayInWeek)
+					: null,
 		};
 
 		await setAlarm({ data: alarmData }).then(async (res) => {
@@ -146,67 +152,179 @@ function EditAlarm({
 		resolver: zodResolver(schema),
 	});
 
+	const alarmDays = (day: string, checked: boolean) => {
+		return (
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					paddingRight: "20px",
+				}}
+			>
+				<Checkbox
+					checked={checked}
+					onChange={() => {
+						null;
+					}}
+				/>
+				<span style={{ marginTop: "2px" }}>{i18n.t(day)}</span>
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<Modal position="middle" hide={hide} onClose={onClose}>
 				<ModalTitle>
 					<h3
 						className="m-0"
-						style={{ marginBottom: "16px", fontWeight: "500" }}
+						style={{ marginBottom: "14px", fontWeight: "500" }}
 					>
-						{i18n.t("Set Device Alarm")}
+						{i18n.t("Set Alarms")}
 					</h3>
 				</ModalTitle>
 				<ModalContent>
 					<FormProvider {...form}>
 						<div
 							style={{
-								height: "300px",
+								height: "350px",
 							}}
 						>
-							{frequency == "Weekly" ? (
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "row",
+									justifyContent: "flex-start",
+									alignItems: "start",
+								}}
+							>
+								<Switch
+									checked={doseReminder}
+									onChange={() => {
+										setDoseReminder(!doseReminder);
+									}}
+								/>
 								<div
 									style={{
 										display: "flex",
-										flexDirection: "row",
+										flexDirection: "column",
 									}}
 								>
-									<div
+									<span
+										className="m-0"
 										style={{
-											width: "300px",
-											marginBottom: "20px",
-											marginRight: "10px",
+											marginBottom: "5px",
+											fontWeight: "400",
+											fontSize: "24px",
 										}}
 									>
-										<FilterField
-											label={i18n.t(
-												"Select Day of the Dose",
-											)}
-											options={daysInWeek}
-											name="dayInWeek"
-											type="select"
-										/>
-									</div>
-									<FilterField
-										label={i18n.t("Time")}
-										name="nextDoseTime"
-										type="time"
-									/>
+										{i18n.t("Dose Reminder")}
+									</span>
+									<span
+										className="m-0"
+										style={{
+											marginBottom: "16px",
+											fontWeight: "300",
+											fontSize: "16px",
+											fontStyle: "italic",
+										}}
+									>
+										{i18n.t(
+											"Set a daily or weekly alarm based on the appropriate treatment regimen",
+										)}
+									</span>
 								</div>
-							) : frequency == "Daily" ? (
+							</div>
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "row",
+								}}
+							>
+								<FilterField
+									label={i18n.t("Alarm Time")}
+									name="nextDoseTime"
+									type="time"
+									width="170px"
+								/>
 								<div
 									style={{
+										width: "350px",
 										marginBottom: "20px",
+										marginLeft: "20px",
 									}}
 								>
-									<FilterField
-										label={i18n.t("Next Dose Time")}
-										name="nextDoseTime"
-										type="time"
-										width="300px"
-									/>
+									<span
+										style={{
+											fontSize: "14px",
+											color: "#212934",
+										}}
+									>
+										{i18n.t("Alarm Day(s)")}
+									</span>
+									<div
+										style={{
+											display: "flex",
+										}}
+									>
+										{daysInWeek.map((day: any) => {
+											return alarmDays(
+												day.name,
+												day.code,
+											);
+										})}
+									</div>
 								</div>
-							) : null}
+							</div>
+
+							<div
+								style={{
+									display: "flex",
+									flexDirection: "row",
+									justifyContent: "flex-start",
+									alignItems: "start",
+									marginTop: "20px",
+								}}
+							>
+								<Switch
+									checked={appointmentReminder}
+									onChange={() => {
+										setAppointmentReminder(
+											!appointmentReminder,
+										);
+									}}
+								/>
+								<div
+									style={{
+										display: "flex",
+										flexDirection: "column",
+									}}
+								>
+									<span
+										className="m-0"
+										style={{
+											marginBottom: "5px",
+											fontWeight: "400",
+											fontSize: "24px",
+										}}
+									>
+										{i18n.t("Appointment Reminder")}
+									</span>
+									<span
+										className="m-0"
+										style={{
+											marginBottom: "16px",
+											fontWeight: "300",
+											fontSize: "16px",
+											fontStyle: "italic",
+										}}
+									>
+										{i18n.t(
+											"Set a reminder for an upcoming appointment or refill. Each new reminder must be set manually",
+										)}
+									</span>
+								</div>
+							</div>
 
 							<div
 								style={{
@@ -216,7 +334,7 @@ function EditAlarm({
 							>
 								<div
 									style={{
-										width: "300px",
+										width: "350px",
 										marginBottom: "20px",
 										marginRight: "10px",
 									}}
