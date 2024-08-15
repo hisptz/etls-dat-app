a357d849993127a8b5dfa9374821e049800886f3
ericchingalo <echingalo@gmail.com>
2024-08-15 19:41:41 +0300

    refactor: added the button for unassigning the device from client

--- src/modules/shared/components/ProfileArea/components/EditDevice.tsx
+++ src/modules/shared/components/ProfileArea/components/EditDevice.tsx
@@ -8,10 +8,10 @@ import {
 	ButtonStrip,
 } from "@dhis2/ui";
 import i18n from "@dhis2/d2-i18n";
-import { FilterField } from "../../../Configuration/components/ProgramMapping/components/FilterField";
+import { FilterField } from "../../../../Configuration/components/ProgramMapping/components/FilterField";
 import { useSetting } from "@dhis2/app-service-datastore";
-import { DeviceIMEIList } from "../../constants";
-import { useAssignDevice } from "../utils/assignDevice";
+import { DeviceIMEIList } from "../../../constants";
+import { useAssignDevice } from "../../utils/assignDevice";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
